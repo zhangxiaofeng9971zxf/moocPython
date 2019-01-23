849bf727dd85c6f5b4e07f449a9d20c0de938cc7
zhangxiaofeng9971zxf <zhangxiaofeng9971@126.com>
2019-01-23 09:46:41 +0800

    利用科赫曲线绘制一个小雪花

---
+++ 科赫雪花.py
@@ -0,0 +1,22 @@
+import turtle
+def koch(size,n):#定义科赫曲线函数
+    if n == 0:
+        turtle.fd(size)
+    else:
+        for angle in [0,60,-120,60]:
+            turtle.left(angle)
+            koch(size/3,n-1)#
+def main():
+    turtle.setup(600,600)
+    turtle.penup()
+    turtle.goto(-200,100)
+    turtle.pendown()
+    turtle.pensize(4)
+    turtle.color('red')
+    level = 3
+    for i in range(3):
+        koch(400,level)
+        turtle.right(120)
+    turtle.hideturtle()
+    turtle.done()
+main()
